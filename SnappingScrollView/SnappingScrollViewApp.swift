22816b2eed6c8a0cea4da4e5dd54ef71a95faa41
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-12-08 10:51:53 +0300

    Initial Commit

---
+++ SnappingScrollView/SnappingScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnappingScrollViewApp.swift
+//  SnappingScrollView
+//
+//  Created by Sajjad Sarkoobi on 8.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnappingScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
